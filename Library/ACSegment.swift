0b624552fffd6ed72d2b6420cd69392091d5ae6b
Christian Ampe <christianampe@icloud.com>
2019-10-03 09:25:14 -0700

    Began adding support for Swift Package Manager as well as Cocoapods and eventually Carthage.

---
+++ Library/ACSegment.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Christian Ampe on 10/2/19.
+//
+
+import Foundation
--- Package.swift
+++ Package.swift
@@ -6,23 +6,26 @@ import PackageDescription
 let package = Package(
     name: "ACSegment",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ACSegment",
-            targets: ["ACSegment"]),
+            targets: ["ACSegmentLibrary"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "ACSegment",
-            dependencies: []),
+            name: "ACSegmentLibrary",
+            path: "Library"),
+        
         .testTarget(
-            name: "ACSegmentTests",
-            dependencies: ["ACSegment"]),
+            name: "ACSegmentTest",
+            dependencies: ["ACSegmentLibrary"],
+            path: "Test"),
+        
+        .target(
+            name: "ACSegmentExample",
+            dependencies: ["ACSegmentLibrary"],
+            path: "Example"),
     ]
 )
